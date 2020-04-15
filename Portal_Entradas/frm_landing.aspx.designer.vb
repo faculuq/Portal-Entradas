ff80babecf8fca74f3ee9a56ebbac447bd7f1c26
faculuq <56322143+faculuq@users.noreply.github.com>
2020-04-15 10:05:01 -0300

    Visual

---
+++ Portal_Entradas/frm_landing.aspx.designer.vb
@@ -0,0 +1,69 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class frm_landing
+
+    '''<summary>
+    '''Control txtNombre.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtNombre As Global.System.Web.UI.HtmlControls.HtmlInputText
+
+    '''<summary>
+    '''Control txtApellido.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtApellido As Global.System.Web.UI.HtmlControls.HtmlInputText
+
+    '''<summary>
+    '''Control txtEmail.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtEmail As Global.System.Web.UI.HtmlControls.HtmlInputText
+
+    '''<summary>
+    '''Control txtTelefono.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtTelefono As Global.System.Web.UI.HtmlControls.HtmlInputText
+
+    '''<summary>
+    '''Control txtMensaje.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtMensaje As Global.System.Web.UI.HtmlControls.HtmlTextArea
+
+    '''<summary>
+    '''Control btn_enviar.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btn_enviar As Global.System.Web.UI.WebControls.Button
+End Class
---
+++ Portal_Entradas/frm_landing.aspx.vb
@@ -0,0 +1,31 @@
+﻿Imports Portal_Entradas_AD
+
+Public Class frm_landing
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Page.IsPostBack = False Then
+            Limpiar()
+        End If
+    End Sub
+
+    Protected Sub btn_enviar_Click(sender As Object, e As EventArgs) Handles btn_enviar.Click
+
+        Dim oMensaje As New cLanding
+
+        If txtNombre.Value <> Nothing And txtApellido.Value <> Nothing And txtEmail.Value <> Nothing And txtTelefono.Value <> Nothing And txtMensaje.Value <> Nothing Then
+            oMensaje.Agregar(txtNombre.Value, txtApellido.Value, txtEmail.Value, txtTelefono.Value, txtMensaje.Value)
+            Limpiar()
+
+        End If
+
+    End Sub
+
+    Private Sub Limpiar()
+        txtApellido.Value = Nothing
+        txtEmail.Value = Nothing
+        txtMensaje.Value = Nothing
+        txtNombre.Value = Nothing
+        txtTelefono.Value = Nothing
+    End Sub
+End Class
